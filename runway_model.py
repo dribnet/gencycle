6db534e37e24066061fd84fa4c50325fda1bd7a1
Tom White <tom@sixdozen.com>
2021-06-24 04:19:05 +0000

    first version of gencycle project

--- runway_model.py
+++ runway_model.py
@@ -1,37 +1,3 @@
-# MIT License
-
-# Copyright (c) 2019 Runway AI, Inc
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-# =========================================================================
-
-# This example contains the minimum specifications and requirements
-# to port a machine learning model to Runway.
-
-# For more instructions on how to port a model to Runway, see the Runway Model
-# SDK docs at https://sdk.runwayml.com
-
-# RUNWAY
-# www.runwayml.com
-# hello@runwayml.com
-
 # =========================================================================
 
 # Import the Runway SDK. Please install it first with
@@ -73,11 +39,14 @@ inputs = {
                 description='Generates a red square when the input text input is "red".')
 def generate(model, args):
     print('[GENERATE] Ran with image "{}"'.format(args['image']))
-    # Generate a PIL or Numpy image based on the input caption, and return it
-    output_image = model.run_on_input(args['image'], args['slices'], args['model'], args['vgg_depth'])
     return {
-        'image': output_image['image'], 'info': output_image['info']
+        'image': arg['image'], 'info': "hello world"
     }
+    # Generate a PIL or Numpy image based on the input caption, and return it
+    # output_image = model.run_on_input(args['image'], args['slices'], args['model'], args['vgg_depth'])
+    # return {
+    #     'image': output_image['image'], 'info': output_image['info']
+    # }
 
 if __name__ == '__main__':
     # run the model server using the default network interface and ports,
